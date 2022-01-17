ec8bdc46ecfc4da4eedf88409f1587721da34b66
Alex Soucy <asoucy22@yahoo.com>
2022-01-16 19:23:41 -0500

    added votes table

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: 'B!ank22!3',
+        password: 'hostpw',
         database: 'election'
 });
 
